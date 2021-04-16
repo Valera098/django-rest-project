d6255fbb5eae42c7747426398aa50622051de7c5
Valera098 <valera098@mail.ru>
2021-04-16 18:04:47 +0300

    Basic api models

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Resort(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, )
+    description = models.TextField()
+    country = models.ForeignKey('Country', on_delete=models.DO_NOTHING)
+    class Meta:
+        ordering = ['-id']
+
+class Country(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    code = models.CharField(max_length=7)
+    iso_code = models.CharField(max_length=2)
+
+class Tour(models.Model):
+    id = models.AutoField(primary_key=True)
+    resort = models.ForeignKey('Resort', on_delete=models.DO_NOTHING)
+    takeoff_time = models.DateTimeField()
+    end_time = models.DateTimeField()
