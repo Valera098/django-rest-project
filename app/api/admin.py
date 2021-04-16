678fbe43225d4811ca4914b2f0ff1f48adeee9d5
Valera098 <valera098@mail.ru>
2021-04-16 21:43:27 +0300

    add import/export and change order status admin-action

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
-from api.models import *
+from api.models import Resort, Country, Tour
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
+class ResortResource(resources.ModelResource):
+    class Meta:
+        model = Resort
 @admin.register(Resort)
-class ResortAdmin(admin.ModelAdmin):
-    pass
+class ResortAdmin(ImportExportModelAdmin):
+    resource_class = ResortResource
 
+class CountryResource(resources.ModelResource):
+    class Meta:
+        model = Country
 @admin.register(Country)
-class CountryAdmin(admin.ModelAdmin):
-    pass
+class CountryAdmin(ImportExportModelAdmin):
+    resource_class = Country
 
+class TourResource(resources.ModelResource):
+    class Meta:
+        model = Tour
 @admin.register(Tour)
-class TourAdmin(admin.ModelAdmin):
-    pass
+class TourAdmin(ImportExportModelAdmin):
+    resource_class = Tour
+
--- app/service/admin.py
+++ app/service/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from service.models import Order
+from import_export import resources
+from import_export.admin import ExportMixin, ImportExportModelAdmin
 
+@admin.action(description="Change status to verified")
+def change_status_true(selfmodeladmin, request, queryset):
+    queryset.update(status=True)
+
+class OrderResource(resources.ModelResource):
+    class Meta:
+        model = Order
 @admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    pass
+class OrderAdmin(ImportExportModelAdmin):
+    resource_class = OrderResource
+    actions = [change_status_true]
